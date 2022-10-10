7048073fa7f6261d42f148448e467b91758cbf92
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2022-10-10 15:56:40 +0600

    assignment 02 done

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="post-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -9,6 +9,28 @@ import PostList from "../../components/Posts/PostList.vue";
 export default {
   components: {
     PostList,
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is out first post",
+            thumbnail:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnqCE5vikGzYMpFtT77iceT-IzbZsMxamQfT_1QUnDRBSXGcct-FsC7pIBwb2BNpaVGzw&usqp=CAU",
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is out second post",
+            thumbnail:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJWPRKIhQt7Y4GbrvXw5A9LlmgTgktae03ag&usqp=CAU",
+          },
+        ],
+      });
+    }, 1500);
   },
 };
 </script>
