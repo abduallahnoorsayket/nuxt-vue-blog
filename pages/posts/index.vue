940bde0bdb1daf0f29bab360d13780d2a908e0f9
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2022-10-11 17:22:23 +0600

    store added

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -12,27 +12,27 @@ export default {
   },
   asyncData(context) {
     return new Promise((resolve, reject) => {
-      // setTimeout(() => {
-      //   resolve({
-      //     loadedPosts: [
-      //       {
-      //         id: "1",
-      //         title: "First Post",
-      //         previewText: "This is out first post",
-      //         thumbnail:
-      //           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnqCE5vikGzYMpFtT77iceT-IzbZsMxamQfT_1QUnDRBSXGcct-FsC7pIBwb2BNpaVGzw&usqp=CAU",
-      //       },
-      //       {
-      //         id: "2",
-      //         title: "Second Post",
-      //         previewText: "This is out second post",
-      //         thumbnail:
-      //           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJWPRKIhQt7Y4GbrvXw5A9LlmgTgktae03ag&usqp=CAU",
-      //       },
-      //     ],
-      //   });
-      // }, 1500);
-      reject(new Error());
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is out first post",
+              thumbnail:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnqCE5vikGzYMpFtT77iceT-IzbZsMxamQfT_1QUnDRBSXGcct-FsC7pIBwb2BNpaVGzw&usqp=CAU",
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is out second post",
+              thumbnail:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJWPRKIhQt7Y4GbrvXw5A9LlmgTgktae03ag&usqp=CAU",
+            },
+          ],
+        });
+      }, 1500);
+      // reject(new Error());
     })
       .then((data) => {
         return data;
@@ -41,6 +41,10 @@ export default {
         // context.error(new Error());
         context.error(e);
       });
+  },
+  created() {
+    this.$store.dispatch("setPosts", this.loadedPosts);
+    console.log(this.$store.getters.loadedPosts);
   },
 };
 </script>
